7508642bfdc1501297f2963f376f6c690392fa3b
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2022-07-01 17:10:33 +0600

    info.js router been setup

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
+const info = require('./routes/info');
 //middleware
 
 app.use(express.json());
+
+//routes
+app.use('/api/v1/info', info);
 
 const port = process.env.PORT || 3000;
 
---
+++ routes/info.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllData, createData } = require('../controllers/info');
+
+router.route('/').get(getAllData);
+router.route('/').post(createData);
+
+module.exports = router;
