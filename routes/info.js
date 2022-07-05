21b2bdb0016ec2f988ae4dea26bce3616aaa3129
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2022-07-05 11:19:26 +0600

    connectDB been setup

--- routes/info.js
+++ routes/info.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { getAllData, createData } = require('../controllers/info');
 
 router.route('/').get(getAllData);
-router.route('/').post(createData);
+router.route('/post').post(createData);
 
 module.exports = router;
